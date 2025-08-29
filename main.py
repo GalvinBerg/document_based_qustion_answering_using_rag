8b7d2bea150ca527dbc2b3c3ec972f6cbaf86026
Joe Galvin Berg V <galvinberg26@gmail.com>
2025-08-29 21:59:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-
-
-
 from retrieval_pipeline import answer_question 
 
 app = FastAPI()
-
 
 @app.post("/ask/")
 async def ask_question(question: str):
